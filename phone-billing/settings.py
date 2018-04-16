610511060a63905c1b9f2c72d8da8ad70dacef3b
Gerardo Soares <ger@rdon.xyz>
2018-04-15 22:07:25 -0300

    Updates DATABASES setting to use postgresql.

--- phone-billing/settings.py
+++ phone-billing/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'phone-billing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'phone-billing',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
     }
 }
 

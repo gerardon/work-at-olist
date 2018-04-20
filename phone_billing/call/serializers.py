750d886a907edde3a8ab16ec584bade2fb41b2a4
Gerardo Soares <ger@rdon.xyz>
2018-04-19 21:05:19 -0300

    Implements CallRecordSerializer utilizing the custom fields.
    This is for create only.

--- phone_billing/call/serializers.py
+++ phone_billing/call/serializers.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
+from .models import Call, CallRecord
 
 
 class PhoneField(serializers.CharField):
@@ -26,3 +29,50 @@ class TimestampField(serializers.DateTimeField):
     def to_internal_value(self, value):
         dt = datetime.utcfromtimestamp(int(value))
         return super().to_internal_value(dt)
+
+
+class CallRecordSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(min_value=0)
+    type = serializers.ChoiceField(choices=CallRecord.RECORD_TYPES,
+                                   source='record_type')
+    timestamp = TimestampField()
+    call_id = serializers.IntegerField(min_value=0)
+    source = PhoneField(source='call.source', required=False)
+    destination = PhoneField(source='call.destination', required=False)
+
+    class Meta:
+        model = CallRecord
+        fields = ('id', 'type', 'timestamp',
+                  'call_id', 'source', 'destination')
+        validators = [
+            UniqueTogetherValidator(
+                queryset=model.objects.all(),
+                fields=('record_type', 'call_id'),
+                message='This call_id already has this type of call record.')]
+
+    def validate(self, data):
+        if data['record_type'] == 'start':
+            if (data.get('call') is None
+                    or 'destination' not in data['call'].keys()
+                    or 'source' not in data['call'].keys()):
+                raise serializers.ValidationError(
+                    'The fields source and destination are required on a call'
+                    ' start record.')
+
+        return super().validate(data)
+
+    def save(self):
+        self.save_related_objects()
+        return super().save()
+
+    def save_related_objects(self):
+        call_data = self.validated_data.pop('call', {})
+
+        call_id = self.validated_data['call_id']
+        source = call_data.get('source', None)
+        destination = call_data.get('destination', None)
+
+        if source and destination:
+            Call.objects.update_or_create(
+                id=call_id, defaults={'source': source,
+                                      'destination': destination})
--- phone_billing/call/tests/test_serializers.py
+++ phone_billing/call/tests/test_serializers.py
@@ -1,9 +1,11 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 from django.utils import timezone
-
 from rest_framework import serializers
 
-from ..serializers import PhoneField, TimestampField
+from ..models import Call
+from ..serializers import PhoneField, TimestampField, CallRecordSerializer
 
 
 class PhoneFieldTestCase(TestCase):
@@ -33,15 +35,134 @@ class PhoneFieldTestCase(TestCase):
 class TimestampFieldTestCase(TestCase):
     def test_to_representation_parses_to_timestamp(self):
         field = TimestampField()
-
         now = timezone.now()
 
         self.assertEquals(field.to_representation(now), int(now.timestamp()))
 
     def test_to_internal_value_parses_timestamp_without_microseconds(self):
         field = TimestampField()
-
         now = timezone.now()
 
         self.assertEquals(field.to_internal_value(now.timestamp()),
                           now.replace(microsecond=0))
+
+
+class CallRecordSerializerTestCase(TestCase):
+    def setUp(self):
+        now = timezone.now()
+
+        self.valid_start_data = {
+            'id': 1,
+            'type': 'start',
+            'timestamp': now.timestamp(),
+            'call_id': 1,
+            'source': '10123456789',
+            'destination': '00123456789'
+        }
+        self.valid_end_data = {
+            'id': 2,
+            'type': 'end',
+            'timestamp': now.timestamp(),
+            'call_id': 1,
+        }
+
+    def test_required_fields(self):
+        serializer = CallRecordSerializer(data={})
+
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('id', serializer.errors)
+        self.assertIn('type', serializer.errors)
+        self.assertIn('timestamp', serializer.errors)
+        self.assertIn('call_id', serializer.errors)
+
+    def test_start_records_require_source_and_destination(self):
+        end_data_copy = self.valid_end_data.copy()
+        end_data_copy['type'] = 'start'
+        serializer = CallRecordSerializer(data=end_data_copy)
+
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('non_field_errors', serializer.errors)
+        self.assertEquals(serializer.errors['non_field_errors'][0],
+                          'The fields source and destination are required on a'
+                          ' call start record.')
+
+    def test_type_choices(self):
+        test_type = 'test'
+        valid_data_copy = self.valid_start_data.copy()
+        valid_data_copy['type'] = test_type
+
+        serializer = CallRecordSerializer(data=valid_data_copy)
+
+        self.assertFalse(serializer.is_valid())
+
+        self.assertIn('type', serializer.errors)
+        self.assertEquals(serializer.errors['type'][0],
+                          f'"{test_type}" is not a valid choice.')
+
+    def test_timestamp_source_and_destination_custom_fields(self):
+        serializer = CallRecordSerializer(data=self.valid_start_data)
+
+        self.assertIsInstance(serializer.fields['timestamp'], TimestampField)
+        self.assertIsInstance(serializer.fields['source'], PhoneField)
+        self.assertIsInstance(serializer.fields['destination'], PhoneField)
+
+    def test_type_and_call_id_unique_together(self):
+        serializer = CallRecordSerializer(data=self.valid_start_data)
+        serializer.is_valid()
+        serializer.save()
+
+        valid_data_copy = self.valid_start_data.copy()
+        valid_data_copy['id'] = 2
+        dup_serializer = CallRecordSerializer(data=valid_data_copy)
+
+        self.assertFalse(dup_serializer.is_valid())
+        self.assertIn('non_field_errors', dup_serializer.errors)
+        self.assertEquals(dup_serializer.errors['non_field_errors'][0],
+                          'This call_id already has this type of call record.')
+
+    def test_start_record_save_related_objects_creates_call_and_removes_call_data(self):
+        serializer = CallRecordSerializer(data=self.valid_start_data)
+
+        self.assertTrue(serializer.is_valid())
+        self.assertEquals(Call.objects.count(), 0)
+
+        serializer.save_related_objects()
+
+        self.assertEquals(Call.objects.count(), 1)
+        call = Call.objects.first()
+        self.assertEquals(call.id, self.valid_start_data['call_id'])
+        self.assertEquals(call.source, self.valid_start_data['source'])
+        self.assertEquals(call.destination,
+                          self.valid_start_data['destination'])
+
+        self.assertNotIn('call', serializer.validated_data)
+
+    @patch.object(CallRecordSerializer, 'save_related_objects')
+    def test_save_calls_save_related_objects(self, mocked_method):
+        serializer = CallRecordSerializer(data=self.valid_start_data)
+
+        self.assertTrue(serializer.is_valid())
+
+        # This reproduces the save_related_objects behaviour.
+        Call.objects.create(id=self.valid_start_data['call_id'],
+                            source=self.valid_start_data['source'],
+                            destination=self.valid_start_data['destination'])
+        serializer.validated_data.pop('call')
+
+        serializer.save()
+
+        mocked_method.assert_called_once_with()
+
+    def test_save_returns_a_correct_call_record(self):
+        serializer = CallRecordSerializer(data=self.valid_start_data)
+
+        self.assertTrue(serializer.is_valid())
+
+        call_record = serializer.save()
+        self.assertEquals(call_record.id, self.valid_start_data['id'])
+        self.assertEquals(call_record.record_type,
+                          self.valid_start_data['type'])
+        self.assertEquals(int(call_record.timestamp.timestamp()),
+                          int(self.valid_start_data['timestamp']))
+        self.assertEquals(call_record.call_id,
+                          self.valid_start_data['call_id'])

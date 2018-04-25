9e51bd2581a2a47f568ad67ac0784ebce3a03f5b
Gerardo Soares <ger@rdon.xyz>
2018-04-25 16:07:39 -0300

    Implements manager for BillRecord that creates it correctly given a call.

--- phone_billing/bill/models.py
+++ phone_billing/bill/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 from ..call.models import Call
 
+from .pricing import calculate_call_charge
+
+
+class BillRecordManager(models.Manager):
+
+    def create_for_call(self, call):
+        return self.create(
+            call_id=call.id,
+            price=calculate_call_charge(call.started_at, call.ended_at)
+        )
+
 
 class BillRecord(models.Model):
+    objects = BillRecordManager()
+
     call = models.ForeignKey(Call, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
--- phone_billing/bill/tests/test_models.py
+++ phone_billing/bill/tests/test_models.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from decimal import Decimal
+from unittest.mock import patch, Mock
 
 from django.db.utils import IntegrityError, DataError
 from django.test import TestCase
@@ -25,3 +26,20 @@ class BillRecordTestCase:
         self.assertEquals(record.id, 1)
         self.assertEquals(record.call, self.call)
         self.assertEquals(record.price, Decimal('1'))
+
+    @patch('phone_billing.bill.models.calculate_call_charge')
+    def test_create_for_call_manager(self, mocked_service):
+        mocked_service.return_value = Decimal('1')
+
+        mock_call = Mock()
+        mock_call.id = self.call.id
+        mock_call.started_at = 'started_at'
+        mock_call.ended_at = 'ended_at'
+
+        record = BillRecord.objects.create_for_call(self.call)
+
+        self.assertEquals(record.id, 1)
+        self.assertEquals(record.call, self.call)
+        self.assertEquals(record.price, Decimal('1'))
+
+        mocked_service.assert_called_once_with('started_at', 'ended_at')
